2e7ff3fe1f223c58c62f78e0194ff3d391bd9b75
Felix <felix.xie@hotmail.com>
2023-01-21 20:55:45 -0500

    created router template for use

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,28 @@
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
 import Navbar from './components/navbar';
+import Home from './pages/homepage';
+import Signup from './pages/signup';
+import JoinGroup from './pages/joingroup';
+import Groups from './pages/groups';
+import Documents from './pages/docs';
+import Upload from './pages/upload';
 
 function App() {
   return (
-    <Navbar />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/joingroup" element={<JoinGroup />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/docs" element={<Documents />} />
+        <Route path="/upload" element={<Upload />} />
+      </Routes>
+
+    </Router>
+    
   );
 }
 
